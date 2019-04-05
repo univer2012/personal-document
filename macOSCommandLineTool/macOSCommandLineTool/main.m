c74085d287dda34166eed478204ee4fc7da66f44
远平 <1084976295@qq.com>
2019-04-05 11:02:57 +0800

    图片

--- macOSCommandLineTool/macOSCommandLineTool/main.m
+++ macOSCommandLineTool/macOSCommandLineTool/main.m
@@ -8,9 +8,15 @@
 
 #import <Foundation/Foundation.h>
 #import "FKApple.h"
+
+#include "stdio.h"
+
 int main(int argc, const char * argv[]) {
     @autoreleasepool {
-        
-
+        float x = 12345.54321;
+        double y = 445566778899.987654321;
+        printf("%f\n",x);
+        printf("%f\n",y);
     }
 }
+
