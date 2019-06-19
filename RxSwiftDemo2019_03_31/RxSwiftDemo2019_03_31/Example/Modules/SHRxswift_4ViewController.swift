1d1f11317be5a32174a858bb2b77763be3968619
huangshengjun@youxin.com <huangshengjun@youxin.com>
2019-06-19 11:08:44 +0800

    添加图片

--- RxSwiftDemo2019_03_31/RxSwiftDemo2019_03_31/Example/Modules/SHRxswift_4ViewController.swift
+++ RxSwiftDemo2019_03_31/RxSwiftDemo2019_03_31/Example/Modules/SHRxswift_4ViewController.swift
@@ -22,11 +22,50 @@ class SHRxswift_4ViewController: UIViewController {
         label.snp.makeConstraints { (make) in
             make.center.equalToSuperview()
         }
-        //Observable序列（每隔0.5秒钟发出一个索引数）
-        let observable = Observable<Int>.interval(1, scheduler: MainScheduler.instance)
-        observable.map{"当前索引数：\($0)"}
-        .bind(to: label.rx.text)//根据索引数不断变放大字体
-        .disposed(by: disposeBag)
+        
+let disposeBag = DisposeBag()
+
+//创建一个PublishSubject
+let subject = PublishSubject<String>()
+
+//由于当前没有任何订阅者，所以这条信息不会输出到控制台
+subject.onNext("111")
+
+//第1次订阅subject
+subject.subscribe(onNext: { (string) in
+    print("第1次订阅：",string)
+}, onCompleted: {
+    print("第1次订阅：onCompleted")
+}).disposed(by: disposeBag)
+
+//当前有1个订阅，则该信息会输出到控制台
+subject.onNext("222")
+
+//第2次订阅subject
+subject.subscribe(onNext: { (string) in
+    print("第2次订阅：",string)
+}, onCompleted: {
+    print("第2次订阅：onCompleted")
+}).disposed(by: disposeBag)
+
+//当前有2个订阅，则该信息会输出到控制台
+subject.onNext("333")
+
+//让subject结束
+subject.onCompleted()
+
+//subject完成后会发出.next事件了。
+subject.onNext("444")
+
+//subject完成后它的所有订阅（包括结束后的订阅），都能收到subject的.completed事件，
+subject.subscribe(onNext: { (string) in
+    print("第3次订阅：",string)
+}, onCompleted: {
+    print("第3次订阅：onCompleted")
+}).disposed(by: disposeBag)
+        
+        
+        
     }
 }
 
