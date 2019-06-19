4c70ec8d57810fe8aeda8d2b31a3dfdbebf770ef
huangshengjun@youxin.com <huangshengjun@youxin.com>
2019-06-19 11:55:14 +0800

    图片

--- RxSwiftDemo2019_03_31/RxSwiftDemo2019_03_31/Example/Modules/SHRxswift_4ViewController.swift
+++ RxSwiftDemo2019_03_31/RxSwiftDemo2019_03_31/Example/Modules/SHRxswift_4ViewController.swift
@@ -25,45 +25,23 @@ class SHRxswift_4ViewController: UIViewController {
         
 let disposeBag = DisposeBag()
 
-//创建一个PublishSubject
-let subject = PublishSubject<String>()
-
-//由于当前没有任何订阅者，所以这条信息不会输出到控制台
-subject.onNext("111")
+//创建一个BehaviorSubject
+let subject = BehaviorSubject(value: "111")
 
 //第1次订阅subject
-subject.subscribe(onNext: { (string) in
-    print("第1次订阅：",string)
-}, onCompleted: {
-    print("第1次订阅：onCompleted")
-}).disposed(by: disposeBag)
-
-//当前有1个订阅，则该信息会输出到控制台
+subject.subscribe { (event) in
+    print("第1次订阅：",event)
+}.disposed(by: disposeBag)
+//发送next事件
 subject.onNext("222")
 
+//发送error事件
+subject.onError(NSError(domain: "local", code: 0, userInfo: nil))
+
 //第2次订阅subject
-subject.subscribe(onNext: { (string) in
-    print("第2次订阅：",string)
-}, onCompleted: {
-    print("第2次订阅：onCompleted")
-}).disposed(by: disposeBag)
-
-//当前有2个订阅，则该信息会输出到控制台
-subject.onNext("333")
-
-//让subject结束
-subject.onCompleted()
-
-//subject完成后会发出.next事件了。
-subject.onNext("444")
-
-//subject完成后它的所有订阅（包括结束后的订阅），都能收到subject的.completed事件，
-subject.subscribe(onNext: { (string) in
-    print("第3次订阅：",string)
-}, onCompleted: {
-    print("第3次订阅：onCompleted")
-}).disposed(by: disposeBag)
-        
+subject.subscribe { (event) in
+    print("第2次订阅：",event)
+}
         
         
     }
