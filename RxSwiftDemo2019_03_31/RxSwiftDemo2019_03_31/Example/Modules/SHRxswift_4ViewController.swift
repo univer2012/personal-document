fe5707a0649a5d2b5047900869dc1dc827a5ba1d
黄胜军 <huangshengjun@youxin.com>
2019-06-24 19:03:35 +0800

    添加图片； 解决 RxSwiftDemo2019_03_31 工程无法运行的问题；

--- RxSwiftDemo2019_03_31/RxSwiftDemo2019_03_31/Example/Modules/SHRxswift_4ViewController.swift
+++ RxSwiftDemo2019_03_31/RxSwiftDemo2019_03_31/Example/Modules/SHRxswift_4ViewController.swift
@@ -11,51 +11,50 @@ import UIKit
 import RxSwift
 import RxCocoa
 
-
-enum MyError: Error {
-    case A
-    case B
-}
-
 class SHRxswift_4ViewController: UIViewController {
+
     let disposeBag = DisposeBag()
     override func viewDidLoad() {
         super.viewDidLoad()
-        //获取第0个频道的歌曲信息
-        getPlaylist("0")
-            .subscribe {event in
-                switch event {
-                case .success(let json):
-                    print("JSON结果：",json)
-                case .error(let error):
-                    print("发生错误：",error)
-                }
-            }
-            .disposed(by: disposeBag)
+        //创建文本输入框
+        let inputFiled = UITextField(frame: CGRect(x: 10, y: 80, width: 200, height: 30))
+        inputFiled.borderStyle = UITextField.BorderStyle.roundedRect
+        self.view.addSubview(inputFiled)
+        
+        //创建文本输入框
+        let outputFiled = UITextField(frame: CGRect(x: 10, y: 150, width: 200, height: 30))
+        outputFiled.borderStyle = UITextField.BorderStyle.roundedRect
+        self.view.addSubview(outputFiled)
+        
+        
+        //创建文本标签
+        let label = UILabel(frame: CGRect(x: 20, y: 190, width: 300, height: 30))
+        self.view.addSubview(label)
+        
+        //创建按钮
+        let button: UIButton = UIButton(type: .custom)
+        button.frame = CGRect(x: 20, y: 230, width: 40, height: 30)
+        button.setTitleColor(.blue, for: .normal)
+        button.setTitleColor(.gray, for: .disabled)
+        button.setTitle("提交", for: .normal)
+        self.view.addSubview(button)
+        
+        //当文本框内容改变
+        let input = inputFiled.rx.text.orEmpty.asDriver()
+        .throttle(0.3)
+        
+        //内容绑定到另一个输入框中
+        input.drive(outputFiled.rx.text)
+        .disposed(by: disposeBag)
+        
+        //内容绑定到文本标签中
+        input.map{ "当前字数：\($0.count)" }
+        .drive(label.rx.text)
+        .disposed(by: disposeBag)
+        
+        //根据内容字数决定按钮是否可用
+        input.map{ $0.count > 5 }
+        .drive(button.rx.isEnabled)
+        .disposed(by: disposeBag)
     }
-}
-//获取豆瓣某频道下的歌曲信息
-func getPlaylist(_ channel: String) -> Single<[String:Any]> {
-    return Single<[String:Any]>.create {single in
-        let url = "https://douban.fm/j/mine/playlist?" + "type=n&channel=\(channel)&from=mainsite"
-        let task = URLSession.shared.dataTask(with: URL(string: url)!, completionHandler: { (data, _, error) in
-            if let error = error {
-                single(.error(error))
-                return
-            }
-            guard let data = data,
-                let json = try? JSONSerialization.jsonObject(with: data, options: .mutableLeaves),
-                let result = json as? [String: Any] else {
-                    single(.error(DataError.cantParseJSON))
-                    return
-            }
-            single(.success(result))
-        })
-        task.resume()
-        return Disposables.create{ task.cancel() }
-    }
-}
-//与数据相关的错误类型
-enum DataError: Error {
-    case cantParseJSON
 }
