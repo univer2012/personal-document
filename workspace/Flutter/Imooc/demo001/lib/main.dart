08848cb87c51cd1c0c6efae2a66a386371d825cb
Sengoln Huang <1084976295@qq.com>
2019-08-27 19:16:13 +0800

    添加flutter demo

--- workspace/Flutter/Imooc/demo001/lib/main.dart
+++ workspace/Flutter/Imooc/demo001/lib/main.dart
@@ -2,6 +2,10 @@ import 'package:flutter/material.dart';
 
 void main() => runApp(MyApp());
 
+
+
+
+
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
@@ -12,8 +16,18 @@ class MyApp extends StatelessWidget {
           title:  Text('TextWidget'),
         ),
         body: Center(
-          child: Text('Hello World imooc!'),
+          child: Container(
+            child: new Image.network(
+              'https://img4.mukewang.com/szimg/5d1032ab08719e0906000338-360-202.jpg',
+              scale: 2.0, //压缩比例
+              repeat: ImageRepeat.repeatY, //repeat：XY重复; repeatX:X重复; repeatY:Y重复
+            ),
+            width: 400.0,
+            height: 300.0,
+            color: Colors.lightBlue,
+          )
         ),
+        
       ),  
     );
   }
