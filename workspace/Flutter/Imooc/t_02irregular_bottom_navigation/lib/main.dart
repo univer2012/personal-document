1468e87efc6773f5789d0040548e69cf41c3fd4d
Sengoln Huang <1084976295@qq.com>
2019-08-30 19:50:17 +0800

    添加demo；

---
+++ workspace/Flutter/Imooc/t_02irregular_bottom_navigation/lib/main.dart
@@ -0,0 +1,17 @@
+import 'package:flutter/material.dart';
+import 'bottom_appBar_demo.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.lightBlue,
+      ),
+      home: BottomAppBarDemo(),
+    );
+  }
+}
