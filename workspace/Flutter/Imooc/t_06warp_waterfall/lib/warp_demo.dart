dac465041eba2503036b68c2f44acfbe33bf5f1d
Sengoln Huang <1084976295@qq.com>
2019-09-09 17:14:15 +0800

    添加demo；

--- workspace/Flutter/Imooc/t_06warp_waterfall/lib/warp_demo.dart
+++ workspace/Flutter/Imooc/t_06warp_waterfall/lib/warp_demo.dart
@@ -12,7 +12,7 @@ class _WarpDemoState extends State<WarpDemo> {
   //初始化状态，给list添加值，这时候调用了一个自定义方法`buildAddButton`
   void initState() {
     super.initState();
-    list = List<Widget>()..add(buildAddBution());
+    list = List<Widget>()..add(buildAddButton());
   }
 
   @override
@@ -25,6 +25,7 @@ class _WarpDemoState extends State<WarpDemo> {
     return Scaffold(
       appBar: AppBar(
         title: Text('Warp流式布局'),
+        backgroundColor: Colors.blueAccent,
       ),
       body: Center(
         child: Opacity(
@@ -44,8 +45,40 @@ class _WarpDemoState extends State<WarpDemo> {
 
   }
 
-  Widget buildAddButon() {
+  Widget buildAddButton() {
+    return GestureDetector(
+      onTap: (){
+        if (list.length < 9) {
+          setState(() {
+            list.insert(list.length - 1, buildPhoto());
+          });
+        }
+      },
+      child: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Container(
+          width: 80.0,
+          height: 80.0,
+          color: Colors.black54,
+          child: Icon(Icons.add),
+        ),
+      ),
+    );
 
+  }
+
+  Widget buildPhoto() {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Container(
+        width: 80.0,
+        height: 80.0,
+        color: Colors.amber,
+        child: Center(
+          child: Text('照片'),
+        ),
+      ),
+    );
   }
 
 }
