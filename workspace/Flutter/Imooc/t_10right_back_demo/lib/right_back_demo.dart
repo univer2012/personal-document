7793812453f1f54e236370f028cc4f6733b765db
Sengoln Huang <1084976295@qq.com>
2019-09-12 16:22:39 +0800

    添加Draggable和ToolTips 组件demo

--- workspace/Flutter/Imooc/t_10right_back_demo/lib/right_back_demo.dart
+++ workspace/Flutter/Imooc/t_10right_back_demo/lib/right_back_demo.dart
@@ -4,7 +4,6 @@ class RightBackDemo extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return CupertinoPageScaffold(
-      //appBar: AppBar(title: Text('首页'),),
       child: Center(
         child: Container(
           height: 100.0,
@@ -13,9 +12,11 @@ class RightBackDemo extends StatelessWidget {
           child: CupertinoButton(
             child: Icon(CupertinoIcons.add),
             onPressed: (){
-              CupertinoPageRoute(builder: (BuildContext context){
+              Navigator.of(context).push(
+                CupertinoPageRoute(builder: (BuildContext context){
                 return RightBackDemo();
-              });
+              })
+              );
             },
           ),
         ),
