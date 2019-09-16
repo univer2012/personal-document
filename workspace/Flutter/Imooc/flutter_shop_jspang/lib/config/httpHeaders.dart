64aa6bafff025e05b4e1d4d7237224f3324d7784
Sengoln Huang <1084976295@qq.com>
2019-09-16 14:50:25 +0800

    伪造请求头获取数据；

---
+++ workspace/Flutter/Imooc/flutter_shop_jspang/lib/config/httpHeaders.dart
@@ -0,0 +1,12 @@
+const httpHeaders = {
+  'Host': 'time.geekbang.org',
+  'User-Agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10.14; rv:69.0) Gecko/20100101 Firefox/69.0',
+  'Accept': 'application/json, text/plain, */*',
+  'Accept-Language': 'zh-CN,zh;q=0.8,zh-TW;q=0.7,zh-HK;q=0.5,en-US;q=0.3,en;q=0.2',
+  'Accept-Encoding': 'gzip, deflate, br',
+  'Connection': 'keep-alive',
+  'Referer': 'https://time.geekbang.org/',
+  'Cookie': 'SERVERID=1fa1f330efedec1559b3abbcb6e30f50|1568614660|1568614351; Hm_lvt_022f847c4e3acd44d4a2481d9187f1e6=1568609051,1568614350,1568614500,1568614661; Hm_lpvt_022f847c4e3acd44d4a2481d9187f1e6=1568614661; _ga=GA1.2.1493566131.1568609051; _gid=GA1.2.1147583504.1568609051; MEIQIA_TRACK_ID=1Qu8KHAYTDSCYSU2jQq8pO0vb6B; MEIQIA_VISIT_ID=1QuJ3rf4oDTlDzATgGaYYssbNut',
+  'Cache-Control': 'max-age=0',
+  'Content-Length': '0',
+};
