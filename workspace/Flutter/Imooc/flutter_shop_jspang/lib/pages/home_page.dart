fdc8ae08ae8ffe6a3a653be0d6e5c1897cb1549c
Sengoln Huang <1084976295@qq.com>
2019-09-16 15:49:43 +0800

    添加flutter_swiper

--- workspace/Flutter/Imooc/flutter_shop_jspang/lib/pages/home_page.dart
+++ workspace/Flutter/Imooc/flutter_shop_jspang/lib/pages/home_page.dart
@@ -1,9 +1,6 @@
-import 'dart:io';
-
 import 'package:flutter/material.dart';
 
-import 'package:dio/dio.dart';
-import '../config/httpHeaders.dart';
+import '../service/service_url.dart';
 
 class HomePage extends StatefulWidget {
   @override
@@ -11,51 +8,24 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
-  TextEditingController typeController = TextEditingController();
-  String showText = "还没有请求数据";
-
+  String homePageContent = '正在获取数据';
 
   @override
+  void initState() {
+    getHomePageContent().then((val){
+      setState(() {
+        homePageContent = val.toString();
+      });
+    });
+    super.initState();
+  }
+  @override
   Widget build(BuildContext context) {
-    return Container(
-      child: Scaffold(
-        appBar: AppBar(title: Text('请求远程数据'),),
-        body: SingleChildScrollView(
-          child: Column(
-            children: <Widget>[
-              RaisedButton(
-                onPressed:_jike,
-                child: Text('请求数据'),
-              ),
-              Text(showText),
-            ],
-          ),
-        ),
+    return Scaffold(
+      appBar: AppBar(title: Text('百姓生活+'),),
+      body: SingleChildScrollView(
+        child: Text(homePageContent),
       ),
     );
   }
-
-  void _jike() {
-    print('开始向极客时间请求数据..........');
-    getHttp().then((val){
-      setState(() {
-        showText = val['data'].toString();
-      });
-    });
-  }
-
-  Future getHttp() async {
-    try{
-      Response response;
-      Dio dio = new Dio();
-      dio.options.headers=httpHeaders;
-      //response = await dio.get('https://time.geekbang.org/serv/v1/column/newAll');
-      response = await dio.get('https://time.geekbang.org/serv/v1/column/topList');
-      print(response);
-      return response.data;
-    }catch(e) {
-      return print(e);
-    }
-  }
-
 }
