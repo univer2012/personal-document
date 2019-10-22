41910983ea5e18db69bc00a1378c4c17cf11e0cc
huangshengjun <1084976295@qq.com>
2019-10-22 11:28:24 +0800

    opt：删除要学习的代码；

--- workspace/Flutter/Imooc/flutter_shop-master/lib/provide/details_info.dart
+++ workspace/Flutter/Imooc/flutter_shop-master/lib/provide/details_info.dart
@@ -6,8 +6,6 @@ import 'dart:convert';
 class DetailsInfoProvide with ChangeNotifier{
   
    DetailsModel goodsInfo =null;
-   bool isLeft = true;
-   bool isRight = false;
 
   //从后台获取商品信息
 
@@ -21,20 +19,6 @@ class DetailsInfoProvide with ChangeNotifier{
       notifyListeners();
     });
    
-
   }
-  //改变tabBar的状态
-  changeLeftAndRight(String changeState){
-    if(changeState=='left'){
-      isLeft=true;
-      isRight=false;
-    }else{
-      isLeft=false;
-      isRight=true;
-    }
-     notifyListeners();
-
-  }
-
 
 }
