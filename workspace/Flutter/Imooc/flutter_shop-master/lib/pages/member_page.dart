a5fbdb1666a11bc0b08b7044f5561d258eecc90c
远平 <1084976295@qq.com>
2019-10-30 09:37:53 +0800

    feat：备份；

--- workspace/Flutter/Imooc/flutter_shop-master/lib/pages/member_page.dart
+++ workspace/Flutter/Imooc/flutter_shop-master/lib/pages/member_page.dart
@@ -45,4 +45,49 @@ class MemberPage extends StatelessWidget {
   }
 
 
+  //我的订单顶部
+  Widget _orderTitle() {
+    return Container(
+      margin: EdgeInsets.only(top: 10),
+      decoration: BoxDecoration(
+        color: Colors.white,
+        border: Border(
+          bottom: BorderSide(width: 1, color: Colors.black12)
+        ),
+      ),
+      child: ListTile(
+        leading: Icon(Icons.list),
+        title: Text('我的订单'),
+        trailing: Icon(Icons.arrow_right),
+      ),
+    );
+  }
+
+
+  //订单列表区域
+
 }
+
+
+// import 'package:flutter/material.dart';
+
+// import 'package:provide/provide.dart';
+// import '../provide/counter.dart';
+
+// class MemberPage extends StatelessWidget {
+//   @override
+//   Widget build(BuildContext context) {
+//     return Scaffold(
+//       body: Center(
+//         child: Provide<Counter>(
+//           builder: (context,child,counter) {
+//             return Text(
+//               '${counter.value}',
+//               style: Theme.of(context).textTheme.display1,
+//             );
+//           },
+//         ),
+//       ),
+//     );
+//   }
+// }
