c01f39a5a1c28298bca06c172fdf20c4aeea8025
远平 <1084976295@qq.com>
2019-11-28 01:27:07 +0800

    add 5 6 practice

--- workspace/PycharmProjects/Demo1/venv/hello_world.py
+++ workspace/PycharmProjects/Demo1/venv/hello_world.py
@@ -1,11 +1,20 @@
 #!/usr/bin/python3
-# -*- coding:utf-8 -*-
 
-#!/usr/bin/python3
+import  sys
 
-# Fibonacci series: 斐波纳契数列
-# 两个元素的总和确定了下一个数
-a, b = 0, 1
-while b < 1000:
-    print(b, end=',')
-    a, b = b, a + b
+def fibonacci(n):    # 生成器函数 - 斐波那契
+    a, b, counter = 0,1,0
+    while True:
+        if (counter > n):
+            return
+        yield  a
+        a, b = b, a + b
+        counter += 1
+
+f = fibonacci(10) # f 是一个迭代器，由生成器返回生成
+
+while True:
+    try:
+        print(next(f), end=' ')
+    except StopIteration:
+        sys.exit()
