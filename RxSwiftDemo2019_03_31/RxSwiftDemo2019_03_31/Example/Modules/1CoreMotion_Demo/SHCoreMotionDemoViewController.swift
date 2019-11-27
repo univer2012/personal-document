c55f723f8bd199cee8a3f9075620fdd8f634ad31
远平 <1084976295@qq.com>
2019-11-27 09:12:10 +0800

    备份；

--- RxSwiftDemo2019_03_31/RxSwiftDemo2019_03_31/Example/Modules/1CoreMotion_Demo/SHCoreMotionDemoViewController.swift
+++ RxSwiftDemo2019_03_31/RxSwiftDemo2019_03_31/Example/Modules/1CoreMotion_Demo/SHCoreMotionDemoViewController.swift
@@ -8,11 +8,33 @@
 
 import UIKit
 
-class SHCoreMotionDemoViewController: UIViewController {
+import CoreMotion
 
+class SHCoreMotionDemoViewController: UIViewController, UIAccelerometerDelegate {
+
+    var ball: UIImageView!
+    var speedX: UIAccelerationValue = 0
+    var speedY: UIAccelerationValue = 0
+    var motionManager = CMMotionManager()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        //放一个小球在中央
+        ball = UIImageView(image: UIImage(named: "ball"))
+        ball.frame = CGRect(x: 0, y: 0, width: 50, height:50)
+        ball.center = self.view.center
+        self.view.addSubview(ball)
+        
+        motionManager.accelerometerUpdateInterval = 1/60
+        
+        if motionManager.isAccelerometerAvailable {
+            let queue = OperationQueue.current!
+            motionManager.startAccelerometerUpdates(to: queue) { (accelerometerData, error) in
+                //动态设置小球位置
+                //self.speedX += accelerometerData.accelera
+                
+            }
+        }
     }
 }
