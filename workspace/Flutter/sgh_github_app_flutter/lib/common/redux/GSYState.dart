d2af93d9e349482a1cb8e98b630f37e3ca315722
huangshengjun <1084976295@qq.com>
2020-03-09 10:15:02 +0800

    sgh_github:圆形头像

--- workspace/Flutter/sgh_github_app_flutter/lib/common/redux/GSYState.dart
+++ workspace/Flutter/sgh_github_app_flutter/lib/common/redux/GSYState.dart
@@ -1,16 +1,21 @@
 import 'package:sgh_github_app_flutter/common/model/User.dart';
 import 'package:sgh_github_app_flutter/common/redux/UserRedux.dart';
+import 'package:sgh_github_app_flutter/common/redux/EventRedux.dart';
+import 'package:sgh_github_app_flutter/widget/EventItem.dart';
 
 class GSYState {
 
   User userInfo;
   
-  GSYState({this.userInfo});
+  List<EventViewModel> eventList = new List();
+
+  GSYState({this.userInfo, this.eventList});
 }
 
 
 GSYState appReducer(GSYState state, dynamic action) {
   return GSYState(
     userInfo: UserReducer(state.userInfo, action),
+    eventList: EventReducer(state.eventList, action),
   );
 }
