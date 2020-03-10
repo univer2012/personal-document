12aff8b00551d7d492aa792bffa2d898f1da96f8
huangshengjun <1084976295@qq.com>
2020-03-10 10:12:15 +0800

    sgh_github:card item

---
+++ workspace/Flutter/sgh_github_app_flutter/lib/widget/GSYCardItem.dart
@@ -0,0 +1,20 @@
+import 'package:flutter/material.dart';
+import 'package:sgh_github_app_flutter/common/style/GSYStyle.dart';
+
+class GSYCardItem extends StatelessWidget {
+  final Widget child;
+  const GSYCardItem({@required this.child});
+
+  @override
+  Widget build(BuildContext context) {
+    return new Card(
+      elevation: 5.0,
+      shape: new RoundedRectangleBorder(
+        borderRadius: BorderRadius.all(Radius.circular(4.0)),
+      ),
+      color: Color(GSYColors.cardWhite),
+      margin: new EdgeInsets.only(left: 10.0, top: 10.0, right: 10.0, bottom: 10.0),
+      child: child,
+    );
+  }
+}
