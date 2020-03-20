ab3dd4d543ef409de025c579d09fc1110606c7e9
远平 <1084976295@qq.com>
2020-03-20 09:19:08 +0800

    sgh_github_app:add user header

--- workspace/Flutter/sgh_github_app_flutter/lib/page/MyPage.dart
+++ workspace/Flutter/sgh_github_app_flutter/lib/page/MyPage.dart
@@ -2,20 +2,36 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_redux/flutter_redux.dart';
 import 'package:sgh_github_app_flutter/common/redux/GSYState.dart';
-// import 'package:sgh_github_app_flutter/widget/userh
+import 'package:sgh_github_app_flutter/widget/UserHeader.dart';
 
-class MyPage extends StatelessWidget {
-  const MyPage({Key key}) : super(key: key);
+class MyPage extends StatefulWidget {
+  MyPage({Key key}) : super(key: key);
+
+  @override
+  _MyPageState createState() => _MyPageState();
+}
+
+class _MyPageState extends State<MyPage> with AutomaticKeepAliveClientMixin {
+
+  @override
+  bool get wantKeepAlive => true;
+
+  @override
+  void initState() { 
+    super.initState();
+  }
+
+  @override
+  void didChangeDependencies() {
+    super.didChangeDependencies();
+  }
 
   @override
   Widget build(BuildContext context) {
     return new StoreConnector<GSYState, String>(
       converter: (store) => store.state.userInfo.name,
       builder: (context, count) {
-        return new Text(
-          count,
-          style: Theme.of(context).textTheme.display1,
-        );
+        return new UserHeaderItem();
       }, 
       
     );
--- workspace/Flutter/sgh_github_app_flutter/lib/widget/EventItem.dart
+++ workspace/Flutter/sgh_github_app_flutter/lib/widget/EventItem.dart
@@ -31,6 +31,7 @@ class EventItem extends StatelessWidget {
               mainAxisSize: MainAxisSize.min,
               children: <Widget>[
                 new Row(
+                  crossAxisAlignment: CrossAxisAlignment.start,
                   children: <Widget>[
                     new ClipOval(
                       child: new FadeInImage.assetNetwork(
@@ -41,7 +42,7 @@ class EventItem extends StatelessWidget {
                         height: 30.0,
                       ),
                     ),
-                    new Padding(padding: EdgeInsets.all(10.0)),
+                    new Padding(padding: EdgeInsets.all(5.0)),
                     new Expanded(child: new Text(eventViewModel.actionUser,style: GSYConstant.smallTextBold,)),
                     new Text(eventViewModel.actionTime,style: GSYConstant.subSmallText,),
                   ],
