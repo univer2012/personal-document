c13c92074fc57e19f89423f4c4a8bd3b73bfc232
huangshengjun <1084976295@qq.com>
2020-04-16 18:50:11 +0800

    rxswift：优化工程结构；

--- RxSwiftDemo2019_03_31/RxSwiftDemo2019_03_31/Contacts/Modules/SHRxswift_1ViewController.swift
+++ RxSwiftDemo2019_03_31/RxSwiftDemo2019_03_31/Contacts/Modules/SHRxswift_1ViewController.swift
@@ -11,6 +11,7 @@ import UIKit
 class SHRxswift_1ViewController: UIViewController {
 
     @IBOutlet weak var greenViewTopConstraint: NSLayoutConstraint!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         
@@ -36,16 +37,5 @@ class SHRxswift_1ViewController: UIViewController {
         
         // Do any additional setup after loading the view.
     }
-    
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
