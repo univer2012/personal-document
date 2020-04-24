4cc97226d57ecbf633ae3670f3857d0fcf43cbf5
huangshengjun <1084976295@qq.com>
2020-04-25 01:11:54 +0800

    移除ObjectiveCDemo20200321 和RxSwiftDemo2019_03_31

--- ObjectiveCDemo20200321/Pods/Target Support Files/Pods-ObjectiveCDemo20200321/Pods-ObjectiveCDemo20200321-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_ObjectiveCDemo20200321 : NSObject
-@end
-@implementation PodsDummy_Pods_ObjectiveCDemo20200321
-@end
--- RxSwiftDemo2019_03_31/Pods/Target Support Files/Pods-RxSwiftDemo2019_03_31/Pods-RxSwiftDemo2019_03_31-dummy.m
+++
@@ -1,5 +0,0 @@
-#import <Foundation/Foundation.h>
-@interface PodsDummy_Pods_RxSwiftDemo2019_03_31 : NSObject
-@end
-@implementation PodsDummy_Pods_RxSwiftDemo2019_03_31
-@end
